f53f4c65b797e18e6e9f7016c77135a93a8d06b7
Yura Chepiga <ura1995ura@gmail.com>
2019-02-03 09:06:54 +0200

    get echo at time from redis

--- modules/Redis.js
+++ modules/Redis.js
@@ -20,6 +20,17 @@ const getHealthUnitCommand = `
     return redis.call('scard', 'APP:HEALTH_CHECK:' .. unitName  .. ':SERVERS')
 `;
 
+const getMessageFromSortedSetCommand = `
+    local key = KEYS[1]
+    local to = KEYS[2]
+    local limit = KEYS[3]
+    local messages = redis.call('ZRANGEBYSCORE', key, 0, to, 'LIMIT', 0, limit)
+    if next(messages) then
+        redis.call('ZREM', key, unpack(messages))
+    end
+    return messages
+`;
+//
 function unitHealthCheck(redis) {
     redis.defineCommand('_unitHealthCheck', {
         numberOfKeys: 3,
@@ -40,10 +51,21 @@ function getHealthUnit(redis) {
     };
 }
 
+function getMessageFromSortedSet(redis) {
+    redis.defineCommand('_getMessageFromSortedSet', {
+        numberOfKeys: 3,
+        lua: getMessageFromSortedSetCommand,
+    });
+    return (key, to, limit) => {
+        return redis._getMessageFromSortedSet(key, to, limit);
+    };
+}
+
 function getConnection() {
     const redis = new Redis(config.get('redis'));
     redis.unitHealthCheck = unitHealthCheck(redis);
     redis.getHealthUnit = getHealthUnit(redis);
+    redis.getMessageFromSortedSet = getMessageFromSortedSet(redis);
     return redis;
 }
 
--- services/cron/checkEcho.js
+++ services/cron/checkEcho.js
@@ -1,5 +1,42 @@
 const log = require('logger');
+const _ = require('lodash');
+const { promiseImmediate } = require('./helper');
+const redis = require('modules/Redis').getConnection();
+const { ECHO_CHUNK_SIZE, ECHO_SORTED_SET_REDIS_KEY } = require('variables');
 
-module.exports.start = function start() {
-    log.info('tick');
+const delayBetweenCalls = 50; // ms
+
+module.exports.start = async function start() {
+    try {
+        const processAmount = await collectTasks();
+        if(processAmount > 0) {
+            log.info(`${new Date().toISOString()} ::: Processed echo ${processAmount}`);
+        }
+    } catch (e) {
+        log.error(e);
+    }
+    setTimeout(start, delayBetweenCalls);
 };
+
+async function collectTasks(processAmount = 0) {
+    if(global.IS_TERMINATED) {
+        return processAmount;
+    }
+    const now = new Date();
+    const messages = await redis
+        .getMessageFromSortedSet(ECHO_SORTED_SET_REDIS_KEY, now.valueOf(), ECHO_CHUNK_SIZE);
+    if(_.isEmpty(messages)) {
+        return processAmount;
+    }
+    processAmount += messages.length;
+    logEcho(messages, now);
+    if(messages.length < ECHO_CHUNK_SIZE) {
+        return processAmount;
+    }
+    return promiseImmediate(collectTasks, processAmount);
+}
+
+function logEcho(messages, now) {
+    _.each(messages, message => log.info(`${now.toISOString()}. Message:${message}`));
+}
+
